4ea4cbf95d52fd809465d1a76bdb1be2cd2372dc
obiPlabon <obiplabon@gmail.com>
2025-03-09 15:48:01 +0600

    Added schema

---
+++ includes/classes/class-schema.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Schema data class.
+ *
+ * @since 8.4.0
+ */
+namespace Directorist;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Schema {
+
+	public static function init() {
+		add_action( 'wp_footer', [ static::class, 'print_schema' ] );
+	}
+
+	public static function print_schema() {
+		$schema = static::get_schema();
+		if ( ! $schema ) {
+			return;
+		}
+
+		$schema = wp_json_encode( $schema );
+		echo '<script type="application/ld+json">' . $schema . '</script>';
+	}
+
+	public static function get_schema() {
+		if ( ! is_singular( ATBDP_POST_TYPE ) ) {
+			return;
+		}
+
+		$directory_id = static::get_listing_directory_id();
+		if ( ! directorist_is_directory( $directory_id ) ) {
+			return [];
+		}
+
+		$data = directorist_get_directory_meta( $directory_id, 'single_listings_contents' );
+		if ( empty( $data ) || empty( $data['fields'] ) ) {
+			return [];
+		}
+
+		$fields = $data['fields'];
+
+		$schema = [
+			'@context'    => 'https://schema.org',
+			'@type'       => 'LocalBusiness',
+			'name'        => get_the_title(),
+			'url'         => get_the_permalink(),
+		];
+
+		if ( isset( $fields['description'] ) ) {
+			$excerpt               = get_post_meta( get_the_ID(), '_excerpt', true );
+			$schema['description'] = empty( $excerpt ) ? get_the_excerpt() : esc_textarea( $excerpt );
+		}
+
+		if ( isset( $fields['map'] ) ) {
+			$schema['geo'] = [
+				'@type'     => 'GeoCoordinates',
+				'latitude'  => esc_html( get_post_meta( get_the_ID(), '_manual_lat', true ) ),
+				'longitude' => esc_html( get_post_meta( get_the_ID(), '_manual_lng', true ) ),
+			];
+		}
+
+		if ( isset( $fields['address'] ) || isset( $fields['zip'] ) ) {
+			$schema['address'] = [
+				'@type'         => 'PostalAddress',
+				'streetAddress' => esc_html( get_post_meta( get_the_ID(), '_address', true ) ),
+				'postalCode'    => esc_html( get_post_meta( get_the_ID(), '_zip', true ) ),
+			];
+		}
+
+		if ( isset( $fields['website'] ) ) {
+			$schema['url'] = esc_url( get_post_meta( get_the_ID(), '_website', true ) );
+		}
+
+		if ( isset( $fields['email'] ) ) {
+			$schema['email'] = get_post_meta( get_the_ID(), '_email', true );
+		}
+
+		if ( isset( $fields['phone'] ) || isset( $fields['phone2'] ) ) {
+			$phone1 = get_post_meta( get_the_ID(), '_phone', true );
+			$phone2 = get_post_meta( get_the_ID(), '_phone2', true );
+
+			if ( $phone1 ) {
+				$schema['contactPoint'][] = [
+					'@type'       => 'ContactPoint',
+					'telephone'   => static::format_phone( $phone1 ),
+					'contactType' => 'customer service',
+				];
+			}
+
+			if ( $phone2 ) {
+				$schema['contactPoint'][] = [
+					'@type'       => 'ContactPoint',
+					'telephone'   => static::format_phone( $phone2 ),
+					'contactType' => 'customer service',
+				];
+			}
+		}
+
+		if ( directorist_is_review_enabled() && directorist_get_listing_review_count( get_the_ID() ) > 0 ) {
+			$schema['aggregateRating'] = [
+				'@type'       => 'AggregateRating',
+				'ratingValue' => directorist_get_listing_rating( get_the_ID() ),
+				'reviewCount' => directorist_get_listing_review_count( get_the_ID() ),
+			];
+		}
+
+		if ( isset( $fields['social_info'] ) ) {
+			$links = get_post_meta( get_the_ID(), '_social', true );
+
+			if ( ! empty( $links ) ) {
+				$schema['sameAs'] = [];
+
+				foreach ( $links as $link ) {
+					if ( empty( $link['url'] ) ) {
+						continue;
+					}
+
+					$schema['sameAs'][] = esc_url( $link['url'] );
+				}
+			}
+		}
+
+		return $schema;
+	}
+
+	protected static function get_listing_directory_id() {
+		return (int) get_post_meta( get_the_ID(),'_directory_type', true );
+	}
+
+	protected static function get_country_code() {
+		if ( class_exists( 'WooCommerce' ) ) {
+			$country = get_option( 'woocommerce_default_country' );
+			$country = explode( ':', $country )[0];
+		} else {
+			$locale  = get_locale();
+			$country = substr( $locale, -2 );
+		}
+
+		$country_codes = [
+			'AF' => '+93',  'AL' => '+355', 'DZ' => '+213', 'AS' => '+1',   'AD' => '+376',
+			'AO' => '+244', 'AI' => '+1',   'AG' => '+1',   'AR' => '+54',  'AM' => '+374',
+			'AW' => '+297', 'AU' => '+61',  'AT' => '+43',  'AZ' => '+994', 'BS' => '+1',
+			'BH' => '+973', 'BD' => '+880', 'BB' => '+1',   'BY' => '+375', 'BE' => '+32',
+			'BZ' => '+501', 'BJ' => '+229', 'BM' => '+1',   'BT' => '+975', 'BO' => '+591',
+			'BA' => '+387', 'BW' => '+267', 'BR' => '+55',  'IO' => '+246', 'BN' => '+673',
+			'BG' => '+359', 'BF' => '+226', 'BI' => '+257', 'KH' => '+855', 'CM' => '+237',
+			'CA' => '+1',   'CV' => '+238', 'KY' => '+1',   'CF' => '+236', 'TD' => '+235',
+			'CL' => '+56',  'CN' => '+86',  'CO' => '+57',  'KM' => '+269', 'CG' => '+242',
+			'CD' => '+243', 'CR' => '+506', 'HR' => '+385', 'CU' => '+53',  'CY' => '+357',
+			'CZ' => '+420', 'DK' => '+45',  'DJ' => '+253', 'DM' => '+1',   'DO' => '+1',
+			'EC' => '+593', 'EG' => '+20',  'SV' => '+503', 'GQ' => '+240', 'ER' => '+291',
+			'EE' => '+372', 'ET' => '+251', 'FJ' => '+679', 'FI' => '+358', 'FR' => '+33',
+			'GA' => '+241', 'GM' => '+220', 'GE' => '+995', 'DE' => '+49',  'GH' => '+233',
+			'GR' => '+30',  'GD' => '+1',   'GT' => '+502', 'GN' => '+224', 'GW' => '+245',
+			'GY' => '+592', 'HT' => '+509', 'HN' => '+504', 'HK' => '+852', 'HU' => '+36',
+			'IS' => '+354', 'IN' => '+91',  'ID' => '+62',  'IR' => '+98',  'IQ' => '+964',
+			'IE' => '+353', 'IL' => '+972', 'IT' => '+39',  'JM' => '+1',   'JP' => '+81',
+			'JO' => '+962', 'KZ' => '+7',   'KE' => '+254', 'KI' => '+686', 'KP' => '+850',
+			'KR' => '+82',  'KW' => '+965', 'KG' => '+996', 'LA' => '+856', 'LV' => '+371',
+			'LB' => '+961', 'LS' => '+266', 'LR' => '+231', 'LY' => '+218', 'LI' => '+423',
+			'LT' => '+370', 'LU' => '+352', 'MO' => '+853', 'MK' => '+389', 'MG' => '+261',
+			'MW' => '+265', 'MY' => '+60',  'MV' => '+960', 'ML' => '+223', 'MT' => '+356',
+			'MH' => '+692', 'MR' => '+222', 'MU' => '+230', 'MX' => '+52',  'FM' => '+691',
+			'MD' => '+373', 'MC' => '+377', 'MN' => '+976', 'ME' => '+382', 'MA' => '+212',
+			'MZ' => '+258', 'MM' => '+95',  'NA' => '+264', 'NR' => '+674', 'NP' => '+977',
+			'NL' => '+31',  'NZ' => '+64',  'NI' => '+505', 'NE' => '+227', 'NG' => '+234',
+			'NO' => '+47',  'OM' => '+968', 'PK' => '+92',  'PW' => '+680', 'PA' => '+507',
+			'PG' => '+675', 'PY' => '+595', 'PE' => '+51',  'PH' => '+63',  'PL' => '+48',
+			'PT' => '+351', 'QA' => '+974', 'RO' => '+40',  'RU' => '+7',   'RW' => '+250',
+			'WS' => '+685', 'SM' => '+378', 'ST' => '+239', 'SA' => '+966', 'SN' => '+221',
+			'RS' => '+381', 'SC' => '+248', 'SL' => '+232', 'SG' => '+65',  'SK' => '+421',
+			'SI' => '+386', 'SB' => '+677', 'SO' => '+252', 'ZA' => '+27',  'ES' => '+34',
+			'LK' => '+94',  'SD' => '+249', 'SR' => '+597', 'SE' => '+46',  'CH' => '+41',
+			'SY' => '+963', 'TW' => '+886', 'TJ' => '+992', 'TZ' => '+255', 'TH' => '+66',
+			'TL' => '+670', 'TG' => '+228', 'TO' => '+676', 'TT' => '+1',   'TN' => '+216',
+			'TR' => '+90',  'TM' => '+993', 'UG' => '+256', 'UA' => '+380', 'AE' => '+971',
+			'GB' => '+44',  'US' => '+1',   'UY' => '+598', 'UZ' => '+998', 'VU' => '+678',
+			'VE' => '+58',  'VN' => '+84',  'YE' => '+967', 'ZM' => '+260', 'ZW' => '+263'
+		];
+
+		return $country_codes[$country] ?? '+1';
+	}
+
+	protected static function format_phone( $phone ) {
+		$phone = preg_replace( '/[^\d+]/', '', $phone );
+
+		if ( strpos( $phone, '00' ) === 0 ) {
+			$phone = '+' . substr( $phone, 2 );
+		}
+
+		if ( $phone[0] !== '+' ) {
+			$phone = static::get_country_code() . $phone;
+		}
+
+		return $phone;
+	}
+}
+
+Schema::init();
